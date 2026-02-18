8e2634bac397735f020c0dffb58378a4c3c09c37
yaGatito <j.lazovskyi@gmail.com>
2026-02-18 04:13:16 +0200

    complete setup of install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ if [ -z "$CONFIG_PATH" ]; then
   exit 1
 fi
 
+mkdir ds-remote && 
+curl -fsSL https://raw.githubusercontent.com/yaGatito/dualsense-override/master/joystick.json | cat > /home/gato/ds-remote/joystick.json 
+curl -fsSL https://raw.githubusercontent.com/yaGatito/dualsense-override/master/ps.sh | cat > /home/gato/ds-remote/ps.sh | chmod +x /home/gato/ds-remote/ps.sh
+
 SERVICE_NAME="dualsense-override"
 
 echo "Installing dependencies..."
@@ -18,7 +22,7 @@ echo "Creating install directory..."
 echo "Downloading main script..."
 sudo curl -fsSL https://raw.githubusercontent.com/yaGatito/dualsense-override/master/ds-remote.sh -o "$APP_PATH/ds-remote.sh"
 
-sudo chmod +x "$APP_PATH/ds-remote.sh"
+sudo chmod +x "$APP_PATH/ds-remote/ds-remote.sh"
 # sudo chown $(whoami):$(whoami) "$APP_PATH/ds-remote.sh"
 
 echo "Creating systemd service..."
